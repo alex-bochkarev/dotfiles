2bb6188ccd41bda1b4256c5de4793c0f308faf70
Alexey Bochkarev <a@bochkarev.io>
2022-12-21 19:29:17 +0100

    fixed regex in fzf-go

--- misc-utils/.local/bin/fzf-go.sh
+++ misc-utils/.local/bin/fzf-go.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-seldest=$(cat ~/.cdg_paths | sed 's/#.*//' | fzf)
+seldest=$(cat ~/.cdg_paths | sed 's/[ ]\?#.*//' | fzf)
 
 if [ ! -z "$seldest" ]; then
     xdotool windowunmap $(xdotool getactivewindow)
     # nohup xdg-open $seldest &
     # nohup alacritty --title "folder-jump" -e nemo $seldest &
-    nohup xdg-open $seldest &
+    nohup xdg-open "$seldest" &
     notify-send -t 500 "jumping..."
 fi
