491fc271c63a99badc049a87f2b273870e3bca98
Alexey Bochkarev <a@bochkarev.io>
2024-04-03 17:51:25 +0200

    waybar disk + cosmetics (backup)

--- borg/.local/bin/borg-cleanup.sh
+++ borg/.local/bin/borg-cleanup.sh
@@ -2,8 +2,8 @@
 
 export BORG_PASSCOMMAND="gpg -q -u aabochkaryov@gmail.com --batch --decrypt $HOME/.password-store/house/borg-workbox.gpg"
 
-echo "Removing the old archives according to the policy..."
+echo "🧹 Removing the old archives according to the policy..."
 borg prune --list --stats --keep-daily=2 --keep-weekly=4 $1 && echo "✅ done."
 
-echo "Freeing up the space..."
+echo "♻ Freeing up the space..."
 borg compact --progress $1 && echo "✅ done."
